433de5046c8223e178c1a9e05e18b45319b59324
Johan Paiz <johanedfb@gmail.com>
2024-04-10 17:08:27 -0600

    Add Test Project

---
+++ zClip-Tests/Tests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace zClip_Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
