7b45f7f37810a5c459671997e27656fa30344f60
Johan Paiz <johanedfb@gmail.com>
2024-04-09 23:47:45 -0600

    Add Singleton SyncService

---
+++ zClip-Desktop/SyncService.cs
@@ -0,0 +1,32 @@
+﻿namespace zClip_Desktop
+{
+    public class SyncService
+    {
+        private static SyncService _instance = null;
+        private static string _deviceIP = null;
+
+        private SyncService(string deviceIP)
+        {
+            _deviceIP = deviceIP;
+        }
+        
+        public static SyncService GetInstance(string deviceIP)
+        {
+            if (_instance == null)
+            {
+                _instance = new SyncService(deviceIP);
+            }
+            return _instance;
+        }
+        
+        public void DestroyInstance()
+        {
+            _instance = null;
+        }
+        
+        public string GetDeviceIP()
+        {
+            return _deviceIP;
+        }
+    }
+}
