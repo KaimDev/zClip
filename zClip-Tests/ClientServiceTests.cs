6c19a3240270dc81a6698e31ec509b391ace00ab
Johan Paiz <johanedfb@gmail.com>
2024-04-23 13:33:20 -0600

    Added Client Service Test for Sent Clipboard with a 202 response.

---
+++ zClip-Tests/ClientServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+using zClip_Desktop.CustomEventArgs;
+using zClip_Desktop.Helpers;
+using zClip_Desktop.Services;
+using zClip_Tests.Mocks;
+
+namespace zClip_Tests
+{
+    public class ClientServiceTests
+    {
+        private ClientService _clientService;
+        private readonly TargetIpAddress _targetIpAddress = new TargetIpAddress("https://google.com");
+
+        [Fact]
+        public async Task Clipboard_Should_Have_Been_Sent_202()
+        {
+            var messageHandlerMock = new MessageHandlerMock(HttpStatusCode.Accepted);
+            _clientService = new ClientService(_targetIpAddress, new HttpClient(messageHandlerMock));
+
+            ClientEventArgs clientEventArgs = new ClientEventArgs();
+            _clientService.OnClientChange += (sender, args) => clientEventArgs = args;
+
+            await _clientService.SendClipboardContent("Test");
+
+            clientEventArgs.StatusCode.Should().Be(202);
+        }
+    }
+}
---
+++ zClip-Tests/Mocks/MessageHandlerMock.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace zClip_Tests.Mocks
+{
+    public class MessageHandlerMock : HttpMessageHandler
+    {
+        private readonly HttpStatusCode _statusCode;
+        
+        public MessageHandlerMock(HttpStatusCode statusCode)
+        {
+            _statusCode = statusCode;
+        }
+        
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(new HttpResponseMessage()
+            {
+                StatusCode = _statusCode
+            });
+        }
+    }
+}
