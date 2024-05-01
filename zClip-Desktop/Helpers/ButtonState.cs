e1b81896995c30d0977f46a031adffa240e4981f
Johan Paiz <johanedfb@gmail.com>
2024-04-30 22:52:47 -0600

    Fixed Missing ButtonState class.

---
+++ zClip-Desktop/Helpers/ButtonState.cs
@@ -0,0 +1,8 @@
+﻿namespace zClip_Desktop.Helpers
+{
+    public static class ButtonState
+    {
+        public static string RequestSync = "Request Sync";
+        public static string CancelSync = "Cancel Sync";
+    }
+}
